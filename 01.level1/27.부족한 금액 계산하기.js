a348d5b30ba70efffb556cddcc6d0c0bc742c9bc
이종경 <gyoung3063413@naver.com>
2024-04-26 19:39:24 +0900

    Add: 부족한 금액 계산하기

---
+++ 01.level1/27.부족한 금액 계산하기.js
@@ -0,0 +1,8 @@
+function solution(price, money, count) {
+  let totalPrice = 0;
+  for (let i = 0; i < count; i++) {
+    totalPrice += price * (i + 1);
+  }
+
+  return totalPrice - money > 0 ? totalPrice - money : 0;
+}
