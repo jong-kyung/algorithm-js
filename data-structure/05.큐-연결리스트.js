ff960a1b4b3e51901011c596293774398560c35e
이종경 <gyoung3063413@naver.com>
2024-05-04 14:11:13 +0900

    Add: 연결리스트로 큐 구현

---
+++ data-structure/05.큐-연결리스트.js
@@ -0,0 +1,51 @@
+//* 연결리스트로 큐 구현
+
+class QueueLinkedList {
+  length = 0;
+  head = null;
+  tail = null;
+
+  enqueue(value) {
+    if (this.head) {
+      this.tail.next = new Node(value);
+      this.tail.next.prev = this.tail;
+      this.tail = this.tail.next;
+    } else {
+      this.head = new Node(value);
+      this.tail = this.head;
+    }
+    this.length++;
+    return this.length;
+  }
+
+  dequeue() {
+    if (this.head) {
+      this.head = this.head?.next;
+      this.length--;
+      return this.head?.prev;
+    }
+  }
+
+  peek() {
+    return this.head;
+  }
+}
+
+class Node {
+  next = null;
+  prev = null;
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+const queue = new QueueLinkedList();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+queue.enqueue(5);
+queue.dequeue();
+queue.dequeue();
+queue.peek();
+queue.peek();
